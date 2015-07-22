9bb71e41c1909c670f485bbfb3a3f041aaeda198
ADYSZY <ADYSZY@CE11704.corp.capgemini.com>
2015-07-22 09:40:48 +0200

    Power method added

--- StarterKit/src/main/java/com/capgemini/StarterKit/MathematicalOperations.java
+++ StarterKit/src/main/java/com/capgemini/StarterKit/MathematicalOperations.java
@@ -1,6 +1,12 @@
 package com.capgemini.StarterKit;
 
 public class MathematicalOperations {
-	int a ;
-	int b; 
+	
+	public static long power(long number, int powerRate) {
+		int result = 1;
+		for (int i = 0; i <= powerRate; i++)
+			result *= number;
+		return result;
+	}
+	
 }
